be154547db7d77186b22718d4ef9aa5abd914cf9
Pat-77 <patricia.alcayaga728@gmail.com>
2025-06-03 19:51:25 +0800

    final project

---
+++ support_ticket_system/user/permissions.py
@@ -0,0 +1,10 @@
+# user/permissions.py
+from rest_framework.permissions import BasePermission
+
+class IsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'admin'
+
+class IsOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == 'admin' or obj.user == request.user
